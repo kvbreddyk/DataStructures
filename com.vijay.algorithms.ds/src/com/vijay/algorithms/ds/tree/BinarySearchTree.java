7a0ffce1050b0e4dfd95885e473fb9564a09fc16
kvbreddyk <kvbreddyk@gmail.com>
2016-02-27 15:02:53 +0530

    2/27/2016

--- com.vijay.algorithms.ds/src/com/vijay/algorithms/ds/tree/BinarySearchTree.java
+++ com.vijay.algorithms.ds/src/com/vijay/algorithms/ds/tree/BinarySearchTree.java
@@ -1,6 +1,7 @@
 package com.vijay.algorithms.ds.tree;
 
 import java.util.Random;
+import java.util.Stack;
 
 public class BinarySearchTree {
 	
@@ -52,5 +53,178 @@ public class BinarySearchTree {
 	public void display() {
 		TreePrinter.print(root);
 	}
+	
+	
+	public void inorder() {
+		inorder(root);
+	}
+	
+	public void preorder() {
+		preorder(root);
+	}
+	
+	public void postorder() {
+		postorder(root);
+	}
+
+	
+	private void inorder(TreeNode current){
+		if(current == null){return;}
+		inorder(current.left);
+		System.out.print(current.data+" ");
+		inorder(current.right);
+	}
+	
+	private void preorder(TreeNode current){
+		if(current == null){return;}
+		System.out.print(current.data+" ");
+		preorder(current.left);
+		preorder(current.right);
+	}
+	
+	private void postorder(TreeNode current){
+		if(current == null){return;}
+		postorder(current.left);
+		postorder(current.right);
+		System.out.print(current.data+" ");
+	}
+
+	public Integer minElement() {
+		return minElement(root);
+		
+	}
+
+	private Integer minElement(TreeNode current) {
+		if(current == null){return null;}
+		if(current.left ==null){return current.data;}
+		return minElement(current.left);
+		
+	}
+
+	public Integer maxElement() {
+		return maxElement(root);
+	}
+
+	private Integer maxElement(TreeNode current) {
+		if(current == null){return null;}
+		if(current.right ==null){return current.data;}
+		return maxElement(current.right);
+	}
+	
+	
+	private int kthSmallest(TreeNode root, int k) {
+	    Stack<TreeNode> stack = new Stack<TreeNode>();
+	 
+	    TreeNode p = root;
+	    int result = 0;
+	 
+	    while(!stack.isEmpty() || p!=null){
+	        if(p!=null){
+	            stack.push(p);
+	            p = p.left;
+	        }else{
+	            TreeNode t = stack.pop();
+	            k--;
+	            if(k==0)
+	                result = t.data;
+	            p = t.right;
+	        }
+	    }
+	 
+	    return result;
+	}
+
+	public Integer select(int k) {
+		return kthSmallest(root,k); 
+		
+	}
+
+//	private Integer select(TreeNode current, int k, Integer count) {
+//		
+//		Integer select = select(current.left, k, count);
+//		if(select !=null){return select;}
+//		
+//		count++;
+//		if(count == k){return current.data;}
+//		
+//		
+//		
+//		
+//		if(current.left !=null){
+//			Integer select = select(current.left, k, count);
+//			if(select !=null){return select;}
+//		}
+//		count++;
+//		if(count == k){
+//			return current.data;
+//		}
+//		
+//		Integer select = select(current.right, k, count);
+//		if(select !=null){return select;}
+//		
+//		return null;
+////		return select(current.right, k, count);
+		
+		
+		
+		
+//		Integer select = select(current.left, k, count);
+//		if(select !=null){return select;}
+//		++count;
+//		if(count == k){
+//			return current.data;
+//		}
+//		
+//		 return select(current.right, k, count);
+		 
+		
+//		Integer select2 = select(current.right, k, count);
+//		if(select2 !=null){return select2;}
+		
+//		return null;
+		
+		
+				
+//		//Move to left most element
+//		if(current.left !=null){
+//			Integer select = select(current.left, k, count);
+//			if(select !=null){
+//				return select;
+//			}
+//			++count;
+//			if(count == k){
+//				return current.data;
+//			}
+//			
+//			Integer select2 = select(current.right, k, count);
+//			if(select2 !=null){
+//				return select2;
+//			}else{
+//				return null;
+//			}
+//			
+//			
+//		}
+//		++count;
+//		if(count == k){
+//			return current.data;
+//		}else{
+//			return null;
+//		}
+//		
+		
+		
+		
+		
+		
+		
+		
+						
+//	}
+
+		
+
+	
+
 
 }
--- com.vijay.algorithms.ds/src/com/vijay/algorithms/ds/tree/Test.java
+++ com.vijay.algorithms.ds/src/com/vijay/algorithms/ds/tree/Test.java
@@ -10,6 +10,17 @@ public class Test {
 		BinarySearchTree bst = new BinarySearchTree();
 		bst.generateBT(10);
 		bst.display();
+		bst.inorder();
+		System.out.println();
+		bst.preorder();
+		System.out.println();
+		bst.postorder();
+//		bst.select(3);
+		System.out.println(String.format("Smallest Element in the Tree is :%s",bst.minElement()));
+		System.out.println(String.format("Maximum Element in the Tree is :%s",bst.maxElement()));
+		
+		int k =10;
+		System.out.println(String.format("%sth smallest in the Tree is :%s",k,bst.select(k)));
 	}
 	
 }
